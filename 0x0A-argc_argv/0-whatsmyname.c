b3c600503c91bef438fc66305601ab9c4222e9a8
Stephenmuhindi <bteneson42@gmail.com>
2023-08-04 02:57:23 -0700

    0-whatsmyname.c 3hours buana

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program name
+ * @argc: arguments number
+ * @argv: arguments array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
